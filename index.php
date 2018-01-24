d654109e639ec666ae905012516bc336af68cd39
Sly Flores <sly@christian.com.ph>
2018-01-24 10:31:15 +0800

    ...

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $q = $con->getData("show tables");
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <title>Models for <?php echo $con->db_name; ?></title>
 	<style style="text/css">
